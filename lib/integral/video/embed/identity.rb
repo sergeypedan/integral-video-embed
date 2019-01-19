e008b6652f996b8f0675a06373a3640db8f36c82
Sergey Pedan <sergey.pedan@gmail.com>
2019-01-20 03:47:56 +0700

    Added Gemsmith skeleton.
    Built with Gemsmith 12.4.0.

---
+++ lib/integral/video/embed/identity.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+
+module Integral
+  module Video
+    module Embed
+      # Gem identity information.
+      module Identity
+        def self.name
+          "integral-video-embed"
+        end
+
+        def self.label
+          "Integral Video Embed"
+        end
+
+        def self.version
+          "0.1.0"
+        end
+
+        def self.version_label
+          "#{label} #{version}"
+        end
+      end
+    end
+  end
+end
